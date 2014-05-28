b3deb2e8af83bcdea6e3112fe89821f78f6de7dc
Domagoj Bulat <domagoj.bulat.zg@gmail.com>
2014-05-28 23:37:16 +0200

    fork

--- echo_tcp.c
+++ echo_tcp.c
@@ -10,39 +10,90 @@
 
 #include "uloop.h"
 #include "usock.h"
+#include "list.h"
 
 static struct uloop_fd server;
 static const char *port = "10000";
 
+static struct list_head childs = LIST_HEAD_INIT(childs);
+
+
+static void child_end(struct uloop_process *uproc, int ret)
+{
+	fprintf(stderr, "child end: %d\n", uproc->pid);
+	free(uproc);
+}
+
+static int child_dostuff(int sfd, struct sockaddr_in client_addr)
+{
+	char buffer[256], *ip;
+	int n;
+	pid_t pid;
+	
+	while ((n = read( sfd, buffer, 255)) > 0 ) {	
+		pid = getpid();
+		buffer[n] = '\0';
+		ip = inet_ntoa(client_addr.sin_addr);
+		fprintf(stderr, "IP: %s PID: %d	Message: %s\n", ip, pid, buffer);
+		write( sfd, buffer, n);
+	}
+	return 0;
+}
+
+
 static void server_cb(struct uloop_fd *fd, unsigned int events)
 {
 	unsigned int sl = sizeof(struct sockaddr_in);
-	int sfd, n;
+	int sfd, ret;
+	pid_t pid, ppid;
 	
 	struct sockaddr_in client_addr;
-	char buffer[256], *ip;
+	
+	for (;;) {
+	pid = getpid();
+	ppid = getppid();
 
 	sfd = accept(server.fd, (struct sockaddr *) &client_addr, &sl);
 	if (sfd < 0) {
-		fprintf(stderr, "Accept failed\n");
 		return;
 	}
 
 	
-	n = read( sfd, buffer, 255);
-	if (n < 0) perror("Error reading from socket");
-	else 
-	{
-		ip = inet_ntoa(client_addr.sin_addr);
-		printf("IP: %s	Message: %s\n", ip, buffer);
-		n = write( sfd, buffer, n);
-		if (n < 0) perror("Error writing to socket");
+	fprintf(stderr, "New conn PID: %d PPID: %d\n", pid, ppid);
+	struct uloop_process *uproc = calloc(1, sizeof(*uproc));
+	if ( !uproc || (uproc->pid = fork()) == -1) {
+		free(uproc);
+		close(sfd);	
 	}
-	close(sfd);
+	if (uproc->pid != 0) {
+		//parent
+		uproc->cb = child_end; // handle za zavrsetak child, za sada
+		uloop_process_add(uproc);
+		close(sfd);
+	}
+	else {	//child do stuff
+		ret = child_dostuff(sfd, client_addr);
+		close(sfd);
+		exit(ret);
+	}
+	}
 }
+
+/*static int close_server(void)
+{
+	struct uloop_process *p, *tmp;
+	list_for_each_entry_safe(p, tmp, &childs, list)	{
+		fprintf(stderr, "pid: %d\n", p->pid);
+	}
+	return 0;
+}*/
 
 static int run_server(void)
 {
+
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	fprintf(stderr, "main PPID: %d PID: %d\n", ppid, pid);
 
 	server.cb = server_cb;
 	server.fd = usock(USOCK_TCP | USOCK_SERVER | USOCK_IPV4ONLY | USOCK_NUMERIC, INADDR_ANY, port);
@@ -54,7 +105,9 @@ static int run_server(void)
 	uloop_init();
 	uloop_fd_add(&server, ULOOP_READ);
 	uloop_run();
-
+	
+//	close_server();
+	uloop_done();
 	return 0;
 }
 
